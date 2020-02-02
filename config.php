af3546255e7ffa733f4bc8c5e6d11c6b04d5a587
JovanaDakic <58692948+JovanaDakic@users.noreply.github.com>
2020-02-02 15:38:10 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+global $SYS_pocdir;
+global $SYS_title;
+global $SYS_baseurl;
+$SYS_pocdir="/itehjma/";
+$SYS_title="E-bookaonica";
+$SYS_description="E-bookaonica opis";
+$SYS_keywords="knjige,biblioteka,books";
+$SYS_baseurl = "localhost";
+require "konekcija.php";
+?>
